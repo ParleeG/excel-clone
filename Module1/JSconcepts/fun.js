6b65e6cf6bdfed9bcb0b2255426fd9daa960cc05
Himanshu Gupta <guptahimanshu0520@gmail.com>
2021-05-17 20:48:39 +0530

    update

---
+++ Module1/JSconcepts/fun.js
@@ -0,0 +1,21 @@
+console.log(a);
+console.log(fun);
+fun();
+
+let a = 20;
+let fun = function(){
+    console.log("Fun Says Hi !!!");
+}
+
+// do this example for on understanding
+// console.log(a);
+// console.log(fun());
+// fun();
+// var a=20;
+// function fun(){
+//     console.log("Fun Says Hi!!");
+// }    
+// //undefined
+// //Fun Says hi!!
+// //undefined
+// //Fun Says hi!!
